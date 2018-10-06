1eb1e8f45684ce9dd6b1b5d66ee69254ef8c0bae
WMXPY <wm@mengw.io>
2018-10-06 13:28:39 -0500

    refactor error - II

--- src/util/error/error.ts
+++ src/util/error/error.ts
@@ -7,26 +7,70 @@
 import * as EST from "estree";
 import { ERROR_CODE, ERROR_LIST } from "marked#declare/error";
 
+export class MarkedError extends Error {
+
+    private _code: number;
+    private _description: string;
+
+    private _info: string | null;
+    private _node: EST.Node | null;
+
+    public constructor(code: number, description: string) {
+
+        super();
+
+        this._code = code;
+        this._description = description;
+
+        this._info = null;
+        this._node = null;
+    }
+
+    public get code(): number {
+        return this._code;
+    }
+
+    public get message(): string {
+
+        const body: string = this._code + ': ' + this._description;
+        return body + (this._info ? (' - ' + this._info) : "");
+    }
+
+    public get node(): EST.Node | null {
+        return this._node;
+    }
+
+    public setInfo(info: string): MarkedError {
+
+        this._info = info;
+        return this;
+    }
+
+    public setNode(node: EST.Node): MarkedError {
+
+        this._node = node;
+        return this;
+    }
+}
+
 /**
  * return new error string object of target error code
  *
  * @param {number} code
  * @returns {Error}
  */
-export const error = (code: ERROR_CODE, info?: string, node?: EST.Node): Error => {
-    const newError: Error = new Error();
-    if (ERROR_LIST[code]) {
-        newError.message = code + ': ' + ERROR_LIST[code] + (info ? (' - ' + info) : '' + (node || ''));
-        newError.name = ERROR_LIST[code];
-        (newError as any).code = code;
+export const error = (code: ERROR_CODE, info?: string, node?: EST.Node): MarkedError => {
 
-        return newError;
-    }
-    newError.message = code + ': ' + ERROR_LIST[code] + (info ? (' - ' + info) : '' + (node || ''));
-    newError.name = ERROR_LIST[9001];
-    (newError as any).code = 9001;
 
-    if ((newError as any).code > 9001) {
+    const newError: MarkedError = Boolean(ERROR_LIST[code])
+        ? new MarkedError(code, ERROR_LIST[code])
+        : new MarkedError(ERROR_CODE.INTERNAL_ERROR, ERROR_LIST[ERROR_CODE.INTERNAL_ERROR]);
+
+    if (info) newError.setInfo(info);
+    if (node) newError.setNode(node);
+
+    if (newError.code > 9001) {
+
         console.log(newError);
     }
 
