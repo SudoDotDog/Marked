56222a37e36c862a538487c17e23d5640426ab82
C <pc@wmpcxpy.com>
2018-10-08 09:21:23 -0500

    [Chore] fix typescript annotation

--- src/variable/trace.ts
+++ src/variable/trace.ts
@@ -8,23 +8,23 @@ import * as EST from "estree";
 import { ITrace } from "marked#declare/variable";
 
 export class Trace implements ITrace {
-    
+
     public static init(): Trace {
-        
+
         return new Trace(null);
     }
 
     private readonly _parent: Trace | null;
-    private readonly _node: EST.Node;
+    private readonly _node: EST.Node | null;
 
-    public constructor(node: EST.Node, parent?: Trace) {
-        
+    public constructor(node: EST.Node | null, parent?: Trace) {
+
         this._parent = parent || null;
         this._node = node;
     }
 
     public stack(node: EST.Node): Trace {
-        
+
         return new Trace(node, this);
     }
 }
