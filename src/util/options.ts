5310eeb7b65adc5a231b05192f7e9d2a7cd128d7
WMXPY <wm@mengw.io>
2018-10-12 21:30:34 -0500

    1.0.0 bug fix

--- src/util/options.ts
+++ src/util/options.ts
@@ -16,22 +16,21 @@ export const getDefaultSandboxOption = (): ISandboxOptions => ({
 
 export const getRawCode = (script: string): string => {
 
-    const rawCode: string = getCommentRemovedCode(script)
-        .split('\n')
-        .map((str: string) => str.trim())
-        .filter((str: string) => Boolean(str))
-        .join('');
-
+    const rawCode: string =
+        getCommentRemovedCode(script)
+            .split('\n')
+            .map((str: string) => str.trim())
+            .filter((str: string) => Boolean(str))
+            .join('');
     return rawCode;
 };
 
-export const getRawCodeLength = (script: string): number => {
-
-    return getRawCode(script).length;
-};
+export const getRawCodeLength =
+    (script: string): number =>
+        getRawCode(script).length;
 
 export const getCommentRemovedCode = (script: string): string => {
-    const regExp: RegExp = /(\/\/.*)|(\/\*[\s\S]*?\*\/)/g;
 
+    const regExp: RegExp = /(\/\/.*)|(\/\*[\s\S]*?\*\/)/g;
     return script.replace(regExp, '');
 };
