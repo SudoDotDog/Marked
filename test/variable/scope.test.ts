2fb3445107b5eeb5a19daf724552686ef7333bad
WMXPY <wm@mengw.io>
2018-10-16 09:49:02 -0500

    implement scope unit test

---
+++ test/variable/scope.test.ts
@@ -0,0 +1,56 @@
+/**
+ * @author WMXPY
+ * @namespace Variable
+ * @description Scope Test
+ */
+
+require('../../src/binding');
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import { ERROR_CODE } from 'marked#declare/error';
+import { VARIABLE_TYPE } from 'marked#declare/variable';
+import { error } from 'marked#util/error/error';
+import { Scope } from 'marked#variable/scope';
+
+describe('Given an Scope class', (): void => {
+
+    const rootScope = Scope.fromRoot();
+    const chance = new Chance('variable-scope');
+
+    it('should be able set config', (): void => {
+
+        const configName: string = chance.string();
+        const configValue: string = chance.string();
+
+        const scope: Scope = rootScope.child();
+        scope.config(configName, configValue);
+
+        expect((scope as any)._configs.get(configName)).to.be.equal(configValue);
+    });
+
+    it('should be able register constant variable', (): void => {
+
+        const name: string = chance.string();
+        const value: number = chance.integer();
+
+        const scope: Scope = rootScope.child();
+        scope.register(VARIABLE_TYPE.CONSTANT)(name, value);
+
+        expect((scope as any)._constantMap.get(name).get()).to.be.equal(value);
+        expect(() => scope.validateEditable(name)).to.be.throw(error(ERROR_CODE.CONSTANT_VARIABLE_CANNOT_BE_EDITED).message);
+    });
+
+    it('should be able register scoped variable', (): void => {
+
+        const name: string = chance.string();
+        const value: number = chance.integer();
+
+        const scope: Scope = rootScope.child();
+        scope.register(VARIABLE_TYPE.SCOPED)(name, value);
+
+        expect((scope as any)._scopeMap.get(name).get()).to.be.equal(value);
+
+        // tslint:disable-next-line
+        expect(() => scope.validateEditable(name)).to.be.not.throw;
+    });
+});
