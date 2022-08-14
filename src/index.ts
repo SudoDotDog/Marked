c633ccea242133f39f2e7f3b81b1cb3e5392a9e6
wmxpy <wm@mengw.io>
2022-08-14 18:17:24 -0500

    export trace methods

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@
  */
 
 export { END_SIGNAL, Evaluator, MarkedResult } from "./declare/evaluate";
-export { IMarkedOptions, OptionName } from "./declare/sandbox";
+export { IMarkedOptions, ModuleResolveResult, OptionName } from "./declare/sandbox";
 export { ScriptLocation } from "./declare/script-location";
 export { EST_TYPE } from "./declare/types";
-export { IExposed } from "./declare/variable";
+export { IExposed, ITrace } from "./declare/variable";
 export * from "./evaluate/evaluate";
 export { Executer } from "./marked/executer";
 export { marked as Marked } from "./marked/marked";
