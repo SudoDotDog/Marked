9d7c20f0f97e14dfe55260dfe7ce2de75fb1f0ea
wmxpy <wm@mengw.io>
2022-09-16 00:47:00 -0500

    fix block statement location not found issue

--- src/debug/node.ts
+++ src/debug/node.ts
@@ -7,8 +7,9 @@
 import { EST_TYPE } from "../declare/types";
 
 const Debug_Skip_Node: Set<EST_TYPE> = new Set([
-    'Program',
-    'Literal',
+    "BlockStatement",
+    "Literal",
+    "Program",
 ] as EST_TYPE[]);
 
 export const shouldDebugNode = (type: EST_TYPE): boolean => {
--- src/debug/snapshot/location.ts
+++ src/debug/snapshot/location.ts
@@ -17,6 +17,8 @@ export class MarkedDebugSnapshotLocation {
         const location: EST.SourceLocation = node.loc as EST.SourceLocation;
         const locationFinder: BaseSourceMapLocationFinder = trace.ensureLocationFinder();
 
+        console.log(node);
+
         const startPosition: MarkedDebugSnapshotPosition =
             MarkedDebugSnapshotPosition.fromPosition(
                 locationFinder.findSourceLocation(location.start, node),
