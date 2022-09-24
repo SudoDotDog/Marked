4947cd41fff0f7cfa6ebc96de972ec369cd06ce7
wmxpy <wm@mengw.io>
2022-09-23 21:48:06 -0500

    add static map support

--- src/variable/native-class/native-class.ts
+++ src/variable/native-class/native-class.ts
@@ -10,23 +10,35 @@ export class MarkedNativeClass {
 
     public static create(
         constructorMethod: MarkedNativeClassConstructor,
+        staticMap: Record<string, any> = {},
     ): MarkedNativeClass {
 
-        return new MarkedNativeClass(constructorMethod);
+        return new MarkedNativeClass(
+            constructorMethod,
+            staticMap,
+        );
     }
 
     private readonly _constructorMethod: MarkedNativeClassConstructor;
+    private _staticMap: Record<string, any>;
 
     private constructor(
         constructorMethod: MarkedNativeClassConstructor,
+        staticMap: Record<string, any>,
     ) {
 
         this._constructorMethod = constructorMethod;
+        this._staticMap = staticMap;
     }
 
     public get constructorMethod(): MarkedNativeClassConstructor {
 
         return this._constructorMethod;
+    }
+
+    public getStaticMember(name: string): any {
+
+        return this._staticMap[name];
     }
 
     public toNative(): any {
