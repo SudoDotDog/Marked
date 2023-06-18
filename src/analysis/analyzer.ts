46919972c97059de4a6fc7040c85c31ed30d565e
wmxpy <wm@mengw.io>
2023-06-17 23:41:25 -0500

    add liter test for location finding of analyzer

--- src/analysis/analyzer.ts
+++ src/analysis/analyzer.ts
@@ -4,6 +4,7 @@
  * @description Analyzer
  */
 
+import * as EST from "estree";
 import { SandboxLanguage } from "../declare/sandbox";
 import { EST_TYPE, IESTreeType } from "../declare/types";
 import { ParseScriptResult } from "../marked/declare";
@@ -37,14 +38,42 @@ export class MarkedAnalyzer {
     ): IESTreeType[T] | null {
 
         const result = findOneESTNodeOrNull(this._parsed.estree, type);
-        return result;
+
+        if (result === null || typeof result === 'undefined') {
+            return null;
+        }
+
+        return {
+            ...result,
+            loc: this._findUpdatedLocation(result),
+        };
     }
 
     public findAllNodes<T extends EST_TYPE>(
         type: T,
     ): Array<IESTreeType[T]> {
 
-        const result = findAllESTNodes(this._parsed.estree, type);
-        return result;
+        const result: Array<IESTreeType[T]> = findAllESTNodes(this._parsed.estree, type);
+
+        return result.map((each: IESTreeType[T]) => ({
+            ...each,
+            loc: this._findUpdatedLocation(each),
+        }));
+    }
+
+    private _findUpdatedLocation(
+        node: EST.Node,
+    ): EST.SourceLocation {
+
+        const originalLocation: EST.SourceLocation = node.loc as EST.SourceLocation;
+
+        const startPosition: EST.Position = this._parsed.locationFinder.findSourceLocation(originalLocation.start, node);
+
+        const endPosition: EST.Position = this._parsed.locationFinder.findSourceLocation(originalLocation.end, node);
+
+        return {
+            start: startPosition,
+            end: endPosition,
+        };
     }
 }
---
+++ test/unit/analysis/analyzer/location.test.ts
@@ -0,0 +1,38 @@
+/**
+ * @author WMXPY
+ * @namespace Analysis_Analyzer
+ * @description Location
+ */
+
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import * as EST from "estree";
+import { MarkedAnalyzer } from '../../../../src/analysis/analyzer';
+import { New_Line_Character } from '../../../../src/host/declare';
+
+describe('Given (Location) Action of {MarkedAnalyzer} Class', (): void => {
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const chance: Chance.Chance = new Chance('analysis-analyzer-location');
+
+    it('should be able to get updated location from source code', async (): Promise<void> => {
+
+        const analyzer = await MarkedAnalyzer.fromSourceAsync([
+            `const a: string = "a";`,
+        ].join(New_Line_Character), 'typescript');
+
+        const node: EST.Literal | null = analyzer.findOneNodeOrNull("Literal");
+
+        expect(node).to.be.not.null;
+        expect(node?.loc).to.be.deep.equal({
+            start: {
+                column: 18,
+                line: 1,
+            },
+            end: {
+                column: 21,
+                line: 1,
+            },
+        });
+    });
+});
