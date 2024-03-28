992cc6bc07474d92da02c29941d926eb849178d0
wmxpy <wm@mengw.io>
2024-03-28 12:55:27 -0500

    Fix bug in login functionality

---
+++ test/integration/sandbox/provide.test.ts
@@ -0,0 +1,44 @@
+/**
+ * @author WMXPY
+ * @namespace Sandbox
+ * @description Provide
+ * @override Integration Test
+ */
+
+/* eslint-disable max-classes-per-file */
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import { Sandbox } from '../../../src';
+import { assertSucceedMarkedResult } from "../../util/assert-result";
+
+describe('Given Integration Sandbox (Provide) Cases', (): void => {
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const chance = new Chance('integration-sandbox-provide');
+
+    const createSandbox = () => {
+        const sandbox: Sandbox = Sandbox.fromAllEvaluators();
+        return sandbox;
+    };
+
+    it('should be able to fail for inject class', async (): Promise<void> => {
+
+        const sandbox: Sandbox = createSandbox();
+
+        let executed: boolean = false;
+        sandbox.provide("test", {
+            "test": () => {
+                executed = true;
+            },
+        });
+
+        const result = await sandbox.evaluate([
+            "import { test } from 'test';",
+            "test();",
+        ].join('\n'));
+
+        assertSucceedMarkedResult(result);
+
+        expect(executed).to.be.true;
+    });
+});
